9f841dc65dd4164be83453fd8dc74272a4691681
karoteeni110 <ansaan1105@gmail.com>
2023-10-20 23:19:45 +0300

    Debug linkedin scraper

--- src-linkedin-scrape/main.py
+++ src-linkedin-scrape/main.py
@@ -2,6 +2,9 @@ from linkedin_scraper import JobSearch, actions
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+import os
+from pprint import pprint
+
 def set_chrome_options() -> Options:
     """Sets chrome options for Selenium.
     Chrome options for headless browser is enabled.
@@ -15,14 +18,18 @@ def set_chrome_options() -> Options:
     chrome_prefs["profile.default_content_settings"] = {"images": 2}
     return chrome_options
 
-def scrape_job_search():
+def scrape_job_search(keyword):
     driver = webdriver.Chrome(options=set_chrome_options())
-    actions.login(driver) # if email and password isnt given, it'll prompt in terminal
-    input("Press Enter")
+    actions.login(driver, os.environ["EMAIL"], os.environ["PWORD"]) # if email and password isnt given, it'll prompt in terminal
+    print("... Logged in.")
     job_search = JobSearch(driver=driver, close_on_complete=False, scrape=False)
 
-    job_listings = job_search.search("Machine Learning Engineer") # returns the list of `Job` from the first page
+    job_listings = job_search.search(keyword) # returns the list of `Job` from the first page
     return job_listings
 
 if __name__ == "__main__":
-    print(scrape_job_search())
+    import pickle 
+    jl = scrape_job_search("data")
+    print(jl)
+    with open("10-20-kw=data-job-listings.pkl", "wb") as handle:
+        pickle.dump(jl, handle, protocol=pickle.HIGHEST_PROTOCOL)
